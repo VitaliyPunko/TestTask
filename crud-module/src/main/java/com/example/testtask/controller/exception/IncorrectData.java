bc1fc5874507b747fcfb7c9af2f2b2d8e4d4f533
Vitaliy Punko <punkovitaliy@gmail.com>
2021-07-04 18:09:42 +0300

    added GlobalExceptionHandler

---
+++ crud-module/src/main/java/com/example/testtask/controller/exception/IncorrectData.java
@@ -0,0 +1,16 @@
+package com.example.testtask.controller.exception;
+
+public class IncorrectData {
+    private String info;
+
+    public IncorrectData() {
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+}
